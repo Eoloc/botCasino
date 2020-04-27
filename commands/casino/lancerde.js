7eb9bb8e7e18aff0029af5fa1fc09f4ed613d9ba
Alexandre Sousa <alexandre.sousa8@etu.univ-lorraine.fr>
2020-04-28 00:42:45 +0200

    nouvelle indication quand il n'y a aucun de

--- commands/casino/lancerde.js
+++ commands/casino/lancerde.js
@@ -16,7 +16,7 @@ class LanceDeCommand extends commando.Command {
         let de = accessDe.get();
         
         if (de.length == 0) {
-            message.reply("Pas de dé créé");
+            message.reply("Pas de dé créé, c!de [nombre de face] pour en créer un");
         }
         else {
             var roll = Math.floor(Math.random() * de[0].face) + 1;
