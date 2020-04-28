dabad918bced2bd74840f5da58b3222324564864
Alexandre Sousa <alexandre.sousa8@etu.univ-lorraine.fr>
2020-04-28 02:10:56 +0200

    mise a jour du module discord

--- commands/casino/de-cree.js
+++ commands/casino/de-cree.js
@@ -1,42 +1,40 @@
-const commando = require('discord.js-commando');
+/* eslint-disable no-mixed-spaces-and-tabs */
+const { Command } = require('discord.js-commando');
 const accessDe = require('./access-de.js');
 
-class DeCreeCommand extends commando.Command {
+module.exports = class DeCreeCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'de',
+			group: 'casino',
+			memberName: 'de',
+			description: '  c!de [nombre de face] | Tire un nombre entre 1 et le nombre que vous avez entré',
+		});
+	}
 
-    constructor(client) {
-        super(client, {
-            name: 'de',
-            group: 'casino',
-            memberName: 'de',
-            description: '  c!de [nombre de face] | Tire un nombre entre 1 et le nombre que vous avez entré'
-        });
-    }
+	async run(message) {
+	    if (message.content.startsWith('c!de ')) {
+	       let argument = message.content.substr('c!de '.length);
+	        argument = parseInt(argument);
+	        if (isNaN(argument) || argument <= 0) {
+	            message.reply('les faces ne sont pas valide');
+	        }
+	        else {
+	            let de = accessDe.get();
+	            console.log('\n=====   CREATION DU DE   =====\n');
+	            de = ([{ face: argument }]);
+	            console.log(de);
+	            accessDe.set(de, (err) => {
+	                if (err) throw err;
+	                console.log('De cree');
+	            });
+	            message.reply('choisit un dé a ' + argument + ' faces');
+	        }
+	    }
+	    else {
+	        message.reply('Erreur d\'utilisitation de la commande : c!de [nombre de face] exemple : c!de 6');
+	    }
 
-    async run(message, args) {
-        if (message.content.startsWith("c!de ")) {
-            var argument = message.content.substr("c!de ".length);
-            argument = parseInt(argument);
-            if (isNaN(argument) || argument <= 0) {
-                message.reply("les faces ne sont pas valide");
-            }
-            else {
-                let de = accessDe.get();
-                console.log("\n=====   CREATION DU DE   =====\n");
-                de = ([{ face: argument }]);
-                console.log(de);
-                accessDe.set(de, (err) => {
-                    if (err) throw err;
-                    console.log('De cree');
-                });
-                message.reply("choisit un dé a " + argument + " faces");
-            }
-        }
-        else {
-            message.reply("Erreur d'utilisitation de la commande : c!de [nombre de face] exemple : c!de 6");
-        }
+	}
 
-    }
-
-}
-
-module.exports = DeCreeCommand;
+};
--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-const commando = require('discord.js-commando');
+const { CommandoClient } = require('discord.js-commando');
 const jsonToken = require('./token.json');
 
-const prefix = "c!";
-const bot = new commando.Client({
-    commandPrefix: prefix
+const bot = new CommandoClient({
+	commandPrefix: 'c!',
+	owner: '188728222240866305',
 });
-var d = new Date();
-var dateActu = d.getUTCDate()+ "/" + (d.getUTCMonth() + 1) + "/" + d.getUTCFullYear();
 
-bot.registry.registerGroup('dev', 'Dev');
-bot.registry.registerGroup('casino', 'Casino');
-bot.registry.registerDefaults();
-bot.registry.registerCommandsIn(__dirname + "/commands");
+const d = new Date();
+const dateActu = d.getUTCDate() + '/' + (d.getUTCMonth() + 1) + '/' + d.getUTCFullYear();
+
+bot.registry.registerGroups([
+	['casino', 'Casino'],
+	['dev', 'Dev'],
+]);
+bot.registry.registerDefaultGroups();
+bot.registry.registerCommandsIn(__dirname + '/commands');
 
 bot.login(jsonToken.token);
 
-bot.on('ready',() => {
-    console.log('\nBot Casino prêt');
-    bot.user.setAvatar(__dirname + "/src/avatar/LaRoulette.jpg")
-    .then(() => console.log('Avatar Casino prêt'))
-    .catch(() => console.error);
-    console.log("Date Casino : " + dateActu);
-    console.log('Log Casino terminé');
-    
+bot.on('ready', () => {
+	console.log('\nBot Casino prêt');
+	bot.user.setAvatar(__dirname + '/src/avatar/LaRoulette.jpg')
+		.then(() => console.log('Avatar Casino prêt'))
+		.catch(() => console.error);
+	console.log('Date Casino : ' + dateActu);
+	console.log('Log Casino terminé');
 });
