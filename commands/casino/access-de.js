f402864db1b614979464fc481e188059ccbd7d01
Alexandre Sousa <alexandre.sousa8@etu.univ-lorraine.fr>
2020-10-28 01:12:06 +0100

    Update access-de.js

--- commands/casino/access-de.js
+++ commands/casino/access-de.js
@@ -1,19 +1,19 @@
-const fs = require('fs')
+const fs = require('fs');
 
 let de = require('./de.json');
 
 module.exports = {
-    get: function () {
-        return de;
-    },
-    getAsync: function (cb) {
-        fs.readFile('commands/casino/de.json', (err, r) => {  
-            if (err) cb(err);
-            cb(null, JSON.parse(r));
-        });
-    },
-    set: function (n, cb) {
-        de = n;
+	get: function() {
+		return de;
+	},
+	getAsync: function(cb) {
+		fs.readFile('commands/casino/de.json', (err, r) => {  
+			if (err) cb(err);
+			cb(null, JSON.parse(r));
+		});
+	},
+	set: function(n, cb) {
+		de = n;
         fs.writeFile('commands/casino/de.json', JSON.stringify(n), (err) => {  
             if (err) cb(err);
             cb(null);
