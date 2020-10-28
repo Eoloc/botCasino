1356528e1a90f13e48824c283c897ee3f09e761d
Alexandre Sousa <alexandre.sousa8@etu.univ-lorraine.fr>
2020-10-28 01:12:01 +0100

    Creation de main pour l'utilisateur ainsi que la banque

--- commands/casino/blackjack.js
+++ commands/casino/blackjack.js
@@ -44,7 +44,7 @@ class BlackJackCommand extends commando.Command {
 		if(estDansBanque) {
 			let paquet = new Array();
 			let couleur = ['pique', 'trefle', 'carreau', 'coeur'];
-			let carte = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'valet', 'dame', 'roi'];
+			let carte = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13'];
 			for(let i = 0; i < 13; i++) {
 				for(let j = 0; j < 4; j++) {
 					paquet.push({ 'carte': carte[i], 'couleur': couleur[j] });
@@ -54,8 +54,24 @@ class BlackJackCommand extends commando.Command {
 			let main = new Array();
 			main.push(paquet.shift());
 			main.push(paquet.shift());
-			console.log(process.cwd());
 			message.reply('votre main : ', { files: ['./src/img/' + main[0].couleur + '/' + main[0].carte + '.jpg', './src/img/' + main[1].couleur + '/' + main[1].carte + '.jpg'] });
+			message.reply('Voulez vous tirer une carte (c!tirercarte) ? (c!stopcarte pour arrêter)');
+			let carte1 = parseInt(main[0].carte);
+			let carte2 = parseInt(main[1].carte);
+			if(carte1 > 10) { carte1 = 10;}
+			if(carte2 > 10) { carte2 = 10;}
+			let score = carte1 + carte2;
+			console.log(score);
+			let mainBanque = new Array();
+			mainBanque.push(paquet.shift());
+			mainBanque.push(paquet.shift());
+			let carteBanque1 = parseInt(mainBanque[0].carte);
+			let carteBanque2 = parseInt(mainBanque[1].carte);
+			if(carteBanque1 > 10) { carteBanque1 = 10;}
+			if(carteBanque2 > 10) { carteBanque2 = 10;}
+			let scoreBanque = carteBanque1 + carteBanque2;
+			console.log(scoreBanque);
+
 		}
 		else {
 			message.reply('Vous n\'avez pas de compte bancaire, b!inscrire pour en créer un');
